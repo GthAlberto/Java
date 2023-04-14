3a3f7ded43293149f84d16a14d08b2e40a74aa38
GthAlberto <127444750+GthAlberto@users.noreply.github.com>
2023-04-14 17:15:04 -0400

    Add files via upload

---
+++ Training/If_else/GradingSystem.java
@@ -0,0 +1,62 @@
+package _02_02;
+
+public class GradingSystem {
+
+  public boolean isAPass(int percentage) {
+    if(percentage >= 60){
+      return true;
+    }
+    else{
+      return false;
+    }
+    // Return true if the percentage is higher than or equal to 60.
+    // Otherwise return false.
+  }
+
+  public char getGrade(int percentage) {
+    if(percentage >= 90){
+      return 'A';
+    }
+    else if(percentage<=89 && percentage>=80){
+      return 'B';
+    }
+    else if (percentage <= 79 && percentage >= 70) {
+      return 'C';
+    }
+    else if (percentage <= 69 && percentage >= 60) {
+      return 'D';
+    }
+    else if (percentage < 60) {
+      return 'E';
+    }
+    // If the percentage is 90 or above, return 'A'.
+    // If it's 80-89, return 'B'.
+    // If it's 70-79, return 'C'.
+    // If it's 60-69, return 'D'.
+    // If it's less than 60, return 'F'.
+    return 'X';
+  }
+
+  public String retakeMessage(int percentage, boolean allowedToRetake) {
+    if(percentage <= 60 && allowedToRetake == true){
+      String string = "The student has been entered for a retake.";
+      return string;
+    }
+    else if(percentage <= 60 && allowedToRetake == false){
+      String string = "The student is not allowed to retake this exam.";
+      return string;
+    }
+    else if(percentage >= 60){
+      String string = "A retake is not required.";
+      return string;
+    }
+    // If percentage is less than 60 and allowedToRetake is true, return a String
+    // that says "The student has been entered for a retake."
+    // If percentage is less than 60 and allowedToRetake is false, return a String
+    // that says "The student is not allowed to retake this exam."
+    // If percentage is 60 or higher, return a String that says "A retake is not
+    // required."
+    return "";
+  }
+
+}
